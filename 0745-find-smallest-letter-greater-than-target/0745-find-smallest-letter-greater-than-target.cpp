a34141d4a14c41b2ad2454b845c12e9d3f657493
Anmol Singh <119476838+Anmol-26@users.noreply.github.com>
2024-07-09 11:11:03 +0530

    Time: 13 ms (45.51%), Space: 19.6 MB (37.7%) - LeetHub

--- 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char ans=letters[0];;
+        char ans=letters[0];
         for(char i: letters){
             if(int(i)>int(target)){
                 ans=i;
